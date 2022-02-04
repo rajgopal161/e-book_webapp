bfb2b7a37feaa0e358e72b52bceb37ca403165e9
rajgopal161 <rajgopalkohli777@gmail.com>
2022-02-04 19:28:08 +0530

    Adding valiations to the signup form and saving the data of the signedup user in db

--- mysite/account/views.py
+++ mysite/account/views.py
@@ -1,3 +1,6 @@
+from email import message
+import re
+from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import UserAuth
@@ -9,8 +12,7 @@ def index(request):
     return render(request,'myapp/index.html' )
 
 def register(request):
-
-
+    
     return render(request,'myapp/register.html')
 
 def register_user(request):
@@ -20,10 +22,28 @@ def register_user(request):
         password = request.POST['password']
         password_confirm = request.POST['password_confirm']
 
-        user = UserAuth(username=username, email=email, password=password)
-        user.save();
-        print("user created")
-        return redirect('/login/')
+        if password == password_confirm:
+            if UserAuth.objects.filter(username=username).exists():
+                print("Username Taken")
+                messages.info(request,"Username already Taken")
+                return redirect('/register/')
+            elif UserAuth.objects.filter(email=email).exists():
+                print("Email Taken")
+                messages.info(request,"Email already exist")
+                return redirect('/register/')
+            else:
+                user = UserAuth(username=username, email=email, password=password)
+                user.save();
+                print("user created")
+                messages.info(request,"Registered Succesfully, Now login")
+                return redirect('/register/')
+
+        else:
+            print("Password not matching")
+            messages.info(request,"Password not matching")
+            return redirect('/register/')
+        
+        
 
 def login(request):
     return render(request,'myapp/login.html')
